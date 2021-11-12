da44c49153dc2f89f60197c5f058a9bfda10d7b5
Joca Koyi <jocakoyi@MacBook-Pro.home>
2021-11-12 12:13:19 +0100

    working subscriptions

--- src/api/graphql-main/resolvers.ts
+++ src/api/graphql-main/resolvers.ts
@@ -19,6 +19,8 @@ export default {
     GetFolders,
   },
   Subscription: {
-    Test: () => pubsub.asyncIterator(['TEST'])
+    Test: {
+      subscribe: () => pubsub.asyncIterator(['TEST'])
+  }
   }
 }
--- src/cmd/main-server.ts
+++ src/cmd/main-server.ts
@@ -1,28 +1,7 @@
-import { default as api } from '../api/graphql-main/main'
-import { ApolloServer } from 'apollo-server-express';
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
+import { default as bootstrap } from './bootstrap'
 const PORT = 3000
-
-const app = express();
-const httpServer = http.createServer(app);
-app.use(cors({origin: "http://localhost", credentials: false}))
-const server = new ApolloServer({
-  typeDefs: api.mainGraphSchemas,
-  resolvers: api.mainGraphResolvers,
-  subscriptions: {
-    onConnect: () => {
-      console.log("Client connected for subscriptions");
-    },
-    onDisconnect: () => {
-      console.log("Client disconnected from subscriptions");
-    },
-  },
-});
-server.applyMiddleware({ app });
-server.installSubscriptionHandlers(httpServer);
+const app = bootstrap()
 app.listen({ port: PORT }, () =>{
-  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
-  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
+  console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`)
+  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}/graphql`)
 })
